d58027baaa6450e0d31e3768ebcd78a7a85d31bc
Ubeng111 <akangvillapuncak@gmail.com>
2026-01-05 11:15:50 +0700

    first t

--- app/apple-app-site-association/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  return new Response(JSON.stringify({"applinks":{"apps":[],"details":[]}}), {
-    headers: { 
-      'Content-Type': 'application/json',
-      'Cache-Control': 'public, s-maxage=3600' 
-    },
-  });
-}
